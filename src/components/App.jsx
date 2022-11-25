cc3cda50d06095e578c40bf9c129b1e9fb2df493
alexander-kulyk <kulyk.alexander@gmail.com>
2022-11-25 18:59:46 +0200

    step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,19 @@ import { nanoid } from 'nanoid'
 
 export class App extends Component {
   state = {
-    contacts: [],
-    name: '',
-    number: ''
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    name:'',
+    number:'',
+    filter:''
   }
 
   handelChangeInput = e =>{
-    
+
     const {value, name } = e.target;
     this.setState({[name]: value });
   }
@@ -29,12 +35,19 @@ export class App extends Component {
     const {name, number, contacts} = this.state
     
     contacts.push({
-      nameUser: name, 
-      idUser: id, 
-      numberUser: number
+      name, 
+      id, 
+      number,
     });
 
     this.rest()
+  }
+
+  handleFindContact = e =>{
+      const {value, name} = e.target
+      this.setState({
+        [name]:value
+      })
   }
 
   rest = () =>{
@@ -54,7 +67,10 @@ export class App extends Component {
             />
         </Section>
         <Section title='Contact'>
-          <Contact state = {this.state}/>
+          <Contact 
+            state = {this.state}
+            handleFindContact = {this.handleFindContact}
+            />
         </Section>  
 
       </Container>
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,19 +1,35 @@
+import { Input } from "components/Form/Form.styled"
 import { ContactList, ItemsContact } from "./ContactList.styled"
 
 
 
 
-export const Contact = ({state}) =>{
-    const {contacts} = state
+export const Contact = ({state, handleFindContact}) =>{
+    const {contacts, filter} = state;
+    const findContact = contacts.filter(({name}) => name.toLowerCase().includes(filter.toLowerCase()));
+    console.log(findContact);
+    
     return(
         <ContactList>
-            {contacts.map(({
-                idUser, 
-                nameUser,
-                numberUser
-            }) =>(
-                <ItemsContact key={idUser}>{nameUser}: {numberUser} </ItemsContact>
-            ))}  
+            <Input
+            type="text"
+            name="filter"
+            value={filter}
+            
+            onChange = {handleFindContact}
+            />
+            {findContact.map(({id, name, number}) =>(
+                <ItemsContact key={id}>{name}: {number} </ItemsContact>
+            ))}
+            
         </ContactList>
     )
 }
+
+// {contacts.map(({
+//     idUser, 
+//     nameUser,
+//     numberUser
+// }) =>(
+//     <ItemsContact key={idUser}>{nameUser}: {numberUser} </ItemsContact>
+// ))}  
